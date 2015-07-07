fd8c96ae9fd6ca358609f6412dc2ad9f7e2b7c51
omskscream <golovkov93@gmail.com>
2015-07-07 14:50:28 +0700

    Updated load and store methods

--- src/main/java/io/github/omskscream/util/Properties.java
+++ src/main/java/io/github/omskscream/util/Properties.java
@@ -1,12 +1,18 @@
 package io.github.omskscream.util;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.io.PrintStream;
 import java.io.PrintWriter;
 import java.io.Reader;
 import java.io.Serializable;
 import java.io.Writer;
+import java.nio.charset.Charset;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
@@ -41,20 +47,46 @@ public class Properties implements Serializable, Cloneable {
         return map.put(key, value);
     }
 
-    public void load(InputStream inStream) {
-        //TODO
+    public void load(InputStream inStream) throws IOException {
+        this.load(new InputStreamReader(inStream));
     }
 
-    public void load(Reader reader) {
-        //TODO
+    public void load(Reader reader) throws IOException {
+        try(BufferedReader r = new BufferedReader(reader)) {
+            r.lines().parallel().forEach(line -> {
+                if (isKeyValueLine(line)) {
+                    String[] keyVal = line.split("[:=]");
+                    if (2 != keyVal.length) {
+                        String[] kv = new String[2];
+                        kv[0] = keyVal[0];
+                        StringBuilder builder = new StringBuilder(keyVal.length - 1);
+                        for (int i = 1; i < keyVal.length; ++i) {
+                            builder.append(keyVal[i]);
+                        }
+                        kv[1] = builder.toString();
+                        keyVal = kv;
+                    }
+                    this.map.put(keyVal[0].trim(), keyVal[1].trim());
+                }
+            });
+        }
     }
 
-    public void store(OutputStream out, String comments) {
-        //TODO
+    public void store(OutputStream out, String comments) throws IOException {
+        this.store(new OutputStreamWriter(out, Charset.forName("UTF-8")), comments);
     }
 
-    public void store(Writer writer, String comments) {
-        //TODO
+    public void store(Writer writer, String comments) throws IOException {
+        try(BufferedWriter w = new BufferedWriter(writer)) {
+            for (Map.Entry<String, String> e : this.map.entrySet()) {
+                w.write(e.getKey()
+                        .replace(" ", "\\ ")
+                        + " = " +
+                        e.getValue());
+                w.newLine();
+            }
+            w.flush();
+        }
     }
 
     public Set<String> propertyNames() {
@@ -87,5 +119,9 @@ public class Properties implements Serializable, Cloneable {
 
     public int size() {
         return map.size();
+    }
+
+    private boolean isKeyValueLine(String line) {
+        return line.matches("^[^#!].+[:=].+");
     }
 }
